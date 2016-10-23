a088af24a7aa21772934931770384cdb1d389ebe
Will Cameron <cameron.will@gmail.com>
2016-10-23 13:03:09 -0700

    eject from default, add OfflinePlugin to test

--- public/service-worker.js
+++ public/service-worker.js
@@ -12,10 +12,10 @@
 */
 
 'use strict';
-
+let package = require('../package.json');
 // Incrementing CACHE_VERSION will kick off the install event and force previously cached
 // resources to be cached again.
-const CACHE_VERSION = 1;
+const CACHE_VERSION = package.version;
 let CURRENT_CACHES = {
   offline: 'offline-v' + CACHE_VERSION
 };
