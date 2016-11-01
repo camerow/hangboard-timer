5f327f019aa4273232517c79a7041af689fd72d1
Will Cameron <cameron.will@gmail.com>
2016-10-31 23:23:12 -0700

    timer working w/out recovery, styling sucks

--- src/HangboardTimer/HangboardTimer.js
+++ src/HangboardTimer/HangboardTimer.js
@@ -31,59 +31,15 @@ export default class HangboardTimer extends Component {
     }
   }
 
-  stopHangRestTimers() {
-    clearInterval(this.hang);
-    clearInterval(this.rest);
-    this.setState({
-      started: false
-    })
-  }
-
-  intervalOrdering() {
-    const ordering = ["hang", "rest"];
-    let index = 0;
-    let head = ordering[index];
-    this.next = function() {
-      if (index > ordering.length - 1) {
-        index = 0;
-      }
-      let next = ordering[index];
-      index += 1;
-      return next;
-    }
-
-    return head;
-  }
-
-  startHangTimer() {
-    const { hang, rest, reps, recover } = this.state.intervals;
-    let self = this;
-
-    this.currentInterval = setInterval(function() {
-      self.setState({
-        currentValue: self.state.intervals[self.state.currentInterval] - 1
-      })
-    }, 1000);
-
-  }
-
-  persistIntervals(intervals) {
-    localStorage.setItem('intervals', JSON.stringify(this.state.intervals));
-  }
-
   incrementInterval(intervalName) {
     let newIntervalState = update(this.state.intervals, {
       [intervalName]: { $apply: function(x) { return x + 1 }}
     });
-
-    let self = this;
-    this.tid = setTimeout(function() {
-      self.persistIntervals(newIntervalState);
-    }, 0);
-    clearTimeout(this.tid);
+    localStorage.setItem('intervals', JSON.stringify(newIntervalState));
 
     this.setState({
-      intervals: newIntervalState
+      intervals: newIntervalState,
+      currentValaue: newIntervalState['hang']
     });
 
   }
@@ -96,11 +52,8 @@ export default class HangboardTimer extends Component {
     let newIntervalState = update(this.state.intervals, {
       [intervalName]: { $apply: function(x) { return x - 1 }}
     });
-    let self = this;
-    this.tid = setTimeout(function() {
-      self.persistIntervals(newIntervalState);
-    }, 0);
-    clearTimeout(this.tid);
+
+    localStorage.setItem('intervals', JSON.stringify(newIntervalState));
 
     this.setState({
       intervals: newIntervalState
@@ -113,6 +66,8 @@ export default class HangboardTimer extends Component {
       <div>
         <div className="row">
           {
+            this.state.started ?
+            null :
             Object.keys(this.state.intervals).map((interval, i) => {
               return (
                 <TimerValueSelect
@@ -127,20 +82,111 @@ export default class HangboardTimer extends Component {
           }
         </div>
         <div className="row">
-          <div>
-            <div style={{ textAlign: "center"}} className="col-xs-12 col-md-8 col-md-offset-2">
-              <h1>{ this.state.currentInterval }</h1>
-              <h1>{ this.state.currentValue }</h1>
-              <h2>{ this.state.intervals.reps } remaing.</h2>
-            </div>
-          </div>
+          <IntervalTimer start={ this.state.started } {...this.state.intervals} />
         </div>
         <div className="row">
           <div className="col-xs-12 col-md-8 col-md-offset-2">
             <TimerButton
             timerRunning={ this.state.started }
-            onStartClick={ () => this.startHangTimer(this.state.intervals.hang, this.state.intervals.rest) } />
+            onStartClick={ () => { this.setState({ started: !this.state.started })}} />
           </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+class IntervalTimer extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  componentWillReceiveProps(nextProps) {
+    let { recover, ...rest } = nextProps;
+
+    this.setState({
+      recover: recover * 60,
+      ...rest
+    })
+
+    if (nextProps.start) {
+      this.startTimer();
+    }
+
+    if (!nextProps.start) {
+      this.stopTimer();
+    }
+  }
+
+  startRest() {
+    this.restTimer = setInterval(() => {
+      if (this.state.rest <= 0) {
+        clearInterval(this.restTimer);
+        this.setState({
+          rest: this.props.rest
+        });
+        this.startHang();
+      } else {
+        this.setState({
+          rest: this.state.rest - 1
+        })
+      }
+    }, 1000);
+  }
+  // these should be minutes
+  startRecovery() {
+    this.recoveryTimer = setInterval(() => {
+      if (this.state.recovery <= 0) {
+        clearInterval(this.recoveryTimer);
+        this.setState({
+          recovery: this.props.recovery
+        })
+      } else {
+        this.setState({
+          recovery: this.state.recovery - 1
+        })
+      }
+    }, 1000);
+  }
+
+  startHang() {
+    this.hangTimer = setInterval(() => {
+      if (this.state.hang <= 0) {
+        clearInterval(this.hangTimer);
+        this.setState({
+          reps: this.state.reps - 1,
+          hang: this.props.hang
+        })
+        this.startRest();
+      } else if (this.state.reps >= 0) {
+        this.setState({
+          hang: this.state.hang - 1
+        })
+      } else {
+        this.startRecovery();
+      }
+    }, 1000);
+  }
+
+  startTimer() {
+    console.log("START", this.state);
+    this.startHang();
+
+  }
+
+  stopTimer() {
+  }
+
+  render() {
+    return (
+      <div style={{ textAlign: "center", fontSize: "50px"}} className="col-xs-12 col-md-8 col-md-offset-2">
+        <div>
+          Reps: {this.state.reps}
+          <br></br>
+          Hang: {this.state.hang}
+          <br></br>
+          Rest: {this.state.rest}
         </div>
       </div>
     );
