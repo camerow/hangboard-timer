91825efd3181c855b93ca2083585ba01c1cd72c4
Will Cameron <cameron.will@gmail.com>
2017-01-04 17:13:24 -0800

    update cache version

--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 'use strict';
 // Incrementing CACHE_VERSION will kick off the install event and force previously cached
 // resources to be cached again.
-const CACHE_VERSION = 5;
+const CACHE_VERSION = 6;
 let CURRENT_CACHES = {
   offline: 'offline-v' + CACHE_VERSION
 };
